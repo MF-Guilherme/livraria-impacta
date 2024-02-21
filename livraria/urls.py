7ebc8e7c5dc049dc54b2593b7b82caff13cdc996
Guilherme Montenegro <guimontenegro23@yahoo.com.br>
2024-02-21 08:36:08 -0300

    Refactor: Alterado o index do sistema para um app exclusivo

--- livraria/urls.py
+++ livraria/urls.py
@@ -4,5 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('autor/', include('autor.urls')),
-    path('livros/', include('livro.urls'))
+    path('livros/', include('livro.urls')),
+    path('', include('index.urls'))
 ]
